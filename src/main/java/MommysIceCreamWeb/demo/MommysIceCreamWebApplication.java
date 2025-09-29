711f96eb862f0d6222b403b38748997180980d04
JefersePasos <dpasos34819@ufide.ac.cr>
2025-09-28 19:01:24 -0600

    Primer commit del proyecto, agrego la configuracion inicial del proyecto a dev y luego a main

---
+++ src/main/java/MommysIceCreamWeb/demo/MommysIceCreamWebApplication.java
@@ -0,0 +1,13 @@
+package MommysIceCreamWeb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MommysIceCreamWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MommysIceCreamWebApplication.class, args);
+	}
+
+}
