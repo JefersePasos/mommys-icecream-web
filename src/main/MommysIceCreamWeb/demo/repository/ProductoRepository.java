ef12c8cdcdc5dbe8659d74582e81d5fae9285b04
Jeff Pasos <dpasos34819@ufide.ac.cr>
2025-11-04 12:35:24 -0600

    Limpieza para poder integrar todas las ramas y probar aca, se subira el las partes del proyetco y se unificaran aca, ya se puede subir el codigo para integrarlo.

--- src/main/MommysIceCreamWeb/demo/repository/ProductoRepository.java
+++
@@ -1,6 +0,0 @@
-﻿package MommysIceCreamWeb.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import MommysIceCreamWeb.demo.model.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {}
