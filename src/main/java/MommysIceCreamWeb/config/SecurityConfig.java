b46ab6388cb4e6cf7a5f41976fda2471ad971937
JefersePasos <dpasos3819@ufide.ac.cr>
2025-11-29 14:30:25 -0600

    Se agrego una animacion del carrito donde se agregan los productos lo cual genera un aviso y tambien agrega la cantidad de productos agregados en el carrito, voy a agregar una parte para hacer el header y que solo sea 1 header y no ir cambiando todas los html

---
+++ src/main/java/MommysIceCreamWeb/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package MommysIceCreamWeb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+            .csrf(csrf -> csrf.disable());  // 💥 desactivar CSRF completamente
+
+        return http.build();
+    }
+}
